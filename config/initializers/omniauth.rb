798cfffd46caf59e41e5f709afa511c82cebc6a5
gautamsawhney <gsawhney21@gmail.com>
2015-04-01 23:58:26 +0530

    changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'user_about_me, manage_pages'}
+  provider :facebook, '375017552676709', '129b0536c7efedb3b5806600af40c0fa',
+           :scope => 'user_about_me, manage_pages'
 end
