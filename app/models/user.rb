a65bcf9332e2d47d31fbe1eadc3a3b84d1d751ea
gautamsawhney <gsawhney21@gmail.com>
2015-04-01 23:22:33 +0530

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ActiveRecord::Base
+  
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+  #     user.provider = auth.provider
+  #     user.uid = auth.uid
+  #     user.name = auth.info.name
+  #     user.email = auth.info.email
+  #     user.profile_pic = auth.info.image
+  #     # user.gender = auth.info.gender
+  #     user.verified_user = auth.info.verified
+  #     user.oauth_token = auth.credentials.token
+  #     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+  #     user.save!
+  #   end
+  # end
+
+  def self.koala(auth)
+    access_token = auth['token']
+    facebook = Koala::Facebook::API.new(access_token)
+    var = facebook.get_object("me?fields=name,picture,accounts{posts.limit(50){id}}")
+    
+    # retrieve collection fo all your managed pages: returns collection of hashes with page id, name, category, access token and permissions
+     # pages = facebook.get_connections('me', 'accounts')
+    # get access token for first page
+     # first_page_token = var.first['access_token']
+
+     # @page_graph = Koala::Facebook::API.new(first_page_token)
+     # @page_graph.get_object('me') # I'm a page
+     # @page_graph.get_connection('me', 'feed') # the page's wall
+
+  end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbPosts::Application.config.secret_token = '98e989ffc1c0bb6a94f75b5cb8e251852166b7c89dcb25cb42b1e62d6ed68898a448169e96648b496ed5a8f1ac2e8dcbf970b58c012d38d0f604918df5821e43'
